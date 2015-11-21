0fd964a08eb83c9c0b5f62a053aa0158220116c5
Ankur Singh <Ankur@Ankurs-MacBook-Pro.local>
2015-11-21 20:31:33 +0530

    Addinf remove completed tasks

--- Todo/js/controllers/maincontroller.js
+++ Todo/js/controllers/maincontroller.js
@@ -1,5 +1,7 @@
 app.controller('myCtrl',['$scope','$element',function($scope,$element){
-  $scope.todo=
+    $scope.todo={};
+  //$scope.todo.tasks=[];  
+    $scope.todo=
   {
     title       : "",
     updateTitle : function(){
@@ -8,19 +10,22 @@ app.controller('myCtrl',['$scope','$element',function($scope,$element){
         //console.log($scope.todo.title);
         }
       },
-    tasks       :  [],
-    update      : function(event){
+    tasks   : (localStorage.getItem('todo')!=null)? JSON.parse(localStorage.getItem('todo')): [],
+    update  :   function(event){
       if(event.keyCode==13 || event.type=='click'){
-      var a= $scope.todo.tasks.indexOf($scope.todo.task)
-      if($scope.todo.task != ""){
+      //var a= $scope.todo.tasks.indexOf($scope.todo.task);
+      if(!!$scope.todo.task && $scope.todo.task!='undefined'){
         $scope.todo.tasks.push({text:$scope.todo.task, done:false});
         }
       $scope.todo.task='';
+        //localStorage.clear();
+      localStorage.setItem('todo',JSON.stringify($scope.todo.tasks));
+
       }
     },
-    changeCss      : function(obj){
+    changeCss   :   function(obj){
       //console.log(obj.confirmed);
-      if(obj.confirmed)
+        if(obj.confirmed)
       {
         obj.myVal= 'line-through';
         obj.done=true;
@@ -29,6 +34,9 @@ app.controller('myCtrl',['$scope','$element',function($scope,$element){
         obj.myVal= 'none';
         obj.done=false;
       }
+      var index= $scope.todo.tasks.indexOf(obj);
+      if(index!=-1)  $scope.todo.tasks[index].done=obj.done;
+        localStorage.setItem('todo',JSON.stringify($scope.todo.tasks));
     },
 
     remainingTasks : function(){
@@ -37,7 +45,24 @@ app.controller('myCtrl',['$scope','$element',function($scope,$element){
       count += todo.done ? 0 : 1;
       });
       return count;
-    }
+    },
+    remove  :   function(){
+      // angular.forEach($scope.todo.tasks, function(todoVar) {
+      //   if(todoVar.done)
+      //   {
+      //     var index=$scope.todo.tasks.indexOf(todoVar);
+      //     $scope.todo.tasks.splice(index,1);
+      //   }
+      // });
+      for (var i = $scope.todo.tasks.length - 1; i >= 0; i--) {
+        if($scope.todo.tasks[i].done)
+        $scope.todo.tasks.splice(i,1);
+        }
+      if(!$scope.todo.tasks.length)
+        localStorage.clear();
+      else
+      localStorage.setItem('todo',JSON.stringify($scope.todo.tasks));
+    } 
 
   }//scope end
 }]);
